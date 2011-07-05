86f55db6c93b76bc6633afa664487a80a2300f4d
Moskalev Mikhail <mmv-ru@yandex.ru>
2011-07-05 22:58:27 +0400

    Russian translation by mmv-ru

---
+++ lang/ru/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['colortheme'] = "Цветовая тема";
+$lang['colortheme_o_dark'] = "Тёмная";
+$lang['colortheme_o_light'] = "Светлая";
+$lang['highlight'] = 'Разрешить подсветку синтаксиса';
+$lang['latex'] = 'Разрешить поддержку плагина LaTeX';
+$lang['wraplimit'] = 'Граница текста по ширине';
+$lang['loadjquery'] = 'Загружать библиотеку jQuery';
